86b9f4c85fb5979a8107d8e94122da751f91db47
Qiushi Chen <qche8284@uni.sydney.edu.au>
2022-09-19 15:39:39 +1000

    frontend style modification done

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus';
+import './style/theme/index.css'
+
+createApp(App).use(ElementPlus).mount('#app')
