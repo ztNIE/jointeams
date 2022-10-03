8d98d6f6de32a19af067057c412211f6193bad3e
yyan3873 <yyan3873@uni.sydney.edu.au>
2022-10-04 04:31:28 +1100

    delete duplicate function
    delete duplicate function getcurrentsemester

--- frontend/src/api/course.js
+++ frontend/src/api/course.js
@@ -4,12 +4,7 @@ const getAllCourse = async function(userId) {
     return await get(`/course/getAllCourse`, {params: {id: userId}})
 }
 
-const getCurrentSemester = async function() {
-    return await get(`/course/findCurrentSemester`, {})
-}
-
 export default {
     name: 'courseAPI',
     getAllCourse,
-    getCurrentSemester
 }
