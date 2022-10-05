0e663909a740a5ab34a39ae28fd291cddac4b055
znie5152 <znie5152@uni.sydney.edu.au>
2022-10-06 09:05:51 +1100

    Tempoprary save

--- frontend/src/views/CourseDetails/CourseDetails.vue
+++ frontend/src/views/CourseDetails/CourseDetails.vue
@@ -1,12 +1,16 @@
 <template>
   <el-card class="box-card">
-    <!--    <user-card v-for="student in currentStudents" :key="student.id"-->
-    <!--    :firstName="student.firstName"-->
-    <!--    :lastName="student.lastName"-->
-    <!--    :id="student.id"-->
-    <!--    :email="student.email"></user-card>-->
     <template #header>
-      {{ this.courseDetail.code }} {{ this.courseDetail.name }}
+<!--      <el-row :gutter="20">-->
+<!--        <el-col :span="20">-->
+<!--          <span>{{ this.courseDetail.code }}</span> {{ this.courseDetail.name }}-->
+<!--        </el-col>-->
+<!--        <el-col :span="4">-->
+<!--          <el-button type="info">Drop</el-button>-->
+<!--          <el-button type="warning">Enroll</el-button>-->
+<!--        </el-col>-->
+<!--      </el-row>-->
+      {{this.courseDetail.code}} {{this.courseDetail.name}}
     </template>
     <el-row :gutter="20" class="full-height">
       <el-col :span="12">
@@ -20,7 +24,21 @@
       </el-col>
       <el-col :span="12">
         <el-empty description="No current student" v-if="isCurrentStudentEmpty"/>
-        <base-card v-for="student in currentStudents" :key="student.id" v-else>
+        <el-row :gutter="20">
+          <el-col :span="18">
+            <div class="student-header">Current Students</div>
+            <div v-if="!isPreviousTeammateEmpty" class="previous-teammate">You have worked with
+              <span class="name" v-for="(teammate, index) in previousTeammates" :key="teammate.id">
+              {{ teammate.firstName }} {{ teammate.lastName }}<span
+                  v-if="index !== Object.keys(previousTeammates).length - 1">, </span>
+              </span>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <el-button type="warning">Find All Groups</el-button>
+          </el-col>
+        </el-row>
+        <base-card v-for="student in currentStudents" :key="student.id">
           <user-card :first-name="student.firstName"
                      :last-name="student.lastName"
                      :email="student.email"
@@ -57,6 +75,9 @@ export default {
     },
     isCurrentStudentEmpty() {
       return !this.currentStudents || this.currentStudents.length === 0;
+    },
+    isPreviousTeammateEmpty() {
+      return !this.previousTeammates || this.previousTeammates.length === 0;
     }
   },
   methods: {
@@ -99,6 +120,7 @@ export default {
           .then((response) => {
             const course = response.data
             _this.courseDetail = {...course}
+            console.log(_this.courseDetail)
           })
     } catch (error) {
       console.log(error)
@@ -107,12 +129,26 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .box-card {
   min-height: 550px;
 }
 
 .full-height {
   height: 100%;
+}
+
+.student-header {
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.previous-teammate {
+  color: #787878;
+
+  & .name {
+    font-weight: normal;
+    color: #232323;
+  }
 }
 </style>
