50c184af942d6841d7336f2c60c7bdc7946ccd5f
znie5152 <znie5152@uni.sydney.edu.au>
2022-10-05 15:50:34 +1100

    Set router for courseDetail

---
+++ frontend/src/views/CourseDetails.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-card class="box-card">
+
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "CourseDetails"
+}
+</script>
+
+<style scoped>
+.box-card {
+  min-height: 550px;
+}
+</style>
